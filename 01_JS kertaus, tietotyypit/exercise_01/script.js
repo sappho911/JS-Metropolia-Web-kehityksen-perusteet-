c16ab323df2d75d89f20623012d9210bc74edd17
Aleksandr Kuzmich <blademaster852@hotmail.com>
2026-08-19 10:36:52 +0300

    almost done

--- 01_JS kertaus, tietotyypit/exercise_01/script.js
+++ 01_JS kertaus, tietotyypit/exercise_01/script.js
@@ -1,7 +1,31 @@
 "use strict";
 
-console.log("test test");
+// console.log("test test");
 
-let html = "<h1>Hello world!</h1>";
+// let html = "<h1>Hello world!</h1>";
 
-document.body.insertAdjacentHTML("beforeend", html);
+// document.body.insertAdjacentHTML("beforeend", html);
+
+const input = +prompt(
+  "Enter temperature in Celsius and it converts it to Fahrenheit.",
+);
+
+console.log(`tepmerature is ${input}`);
+
+const convertToFahrenheit = function (celsius) {
+  if (Number.isNaN(celsius)) {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      "<h1>enter a valid number pls!</h1>",
+    );
+    return;
+  }
+
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  const kelvin = celsius + 273.15;
+
+  const html = `<h1>Converted temperature to fahrenheit  = ${fahrenheit} °F , and Kelvin ${kelvin} K</h1>`;
+  document.body.insertAdjacentHTML("beforeend", html);
+};
+
+convertToFahrenheit(input);
---
+++ 01_JS kertaus, tietotyypit/exercise_02/script.js
@@ -0,0 +1,16 @@
+// console.log('Hello World!');
+
+// const x = prompt("Enter x coordinate using space example: 5 6");
+
+// const parts = x.split(" ");
+
+// console.log(typeof parts);
+
+// const x1 = +parts[0];
+// const y1 = +parts[1];
+// console.log(typeof x1);
+// console.log(typeof y1);
+
+const pistet1 = '2, 5'.split(",");
+
+console.log(typeof pistet1);
